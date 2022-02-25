d6cf0b36ac790bc88bc51f8724904ebfc4529b69
21P31A04I6 <99059691+21P31A04I6@users.noreply.github.com>
2022-02-25 14:44:54 +0530

    #include<stdio.h> int main() {     int num1,num2;     float average;     scanf("%d%d",&num1,&num2);     average=float(num1+num2)/2;     printf("%0.4f",average);     return 0; }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    float average;
+    scanf("%d%d",&num1,&num2);
+    average=float(num1+num2)/2;
+    printf("%0.4f",average);
+    return 0;
+}
